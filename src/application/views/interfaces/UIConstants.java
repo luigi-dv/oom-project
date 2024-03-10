ecaa94bad81b6af3e0a532d6b5a261f0279557c0
codetoby <tobymelcherds@gmail.com>
2024-03-10 09:47:11 +0100

    Add UIConstants class with constants

---
+++ src/application/views/interfaces/UIConstants.java
@@ -0,0 +1,8 @@
+package src.application.views.interfaces;
+
+public class UIConstants {
+    public static final int IMAGE_SIZE = 20;
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 500;
+    public static final int NAV_ICON_SIZE = 20;
+}
