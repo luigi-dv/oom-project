937ee1a24536a15237213e6f0e874979ee36d8aa
codetoby <tobymelcherds@gmail.com>
2024-03-19 13:55:10 +0100

    added search service to get the list of searchables

---
+++ src/application/services/SearchService.java
@@ -0,0 +1,44 @@
+package src.application.services;
+
+import java.util.List;
+
+import src.application.factory.PictureSearchFactory;
+import src.application.factory.UserSearchFactory;
+import src.application.factory.search.Search;
+import src.domain.interfaces.ISearchable;
+import src.domain.util.similarity.Filter;
+import src.domain.util.similarity.picture.SimilarCaption;
+import src.domain.util.similarity.picture.SimiliarHashTags;
+import src.domain.util.similarity.user.SimilarUserName;
+
+public class SearchService {
+
+    private final PictureService pictureService;
+    private final UserService userService;
+
+    public SearchService() {
+        this.pictureService = new PictureService();
+        this.userService = new UserService();
+    }
+
+    @SuppressWarnings("unchecked")
+    public Search search(String searchQuery) {
+        if (searchQuery.startsWith("@")) {
+            Filter filterUsers = new SimilarUserName(searchQuery.substring(1));
+            List<ISearchable> users = (List<ISearchable>) (List<?>) userService.getUsers();
+            return new UserSearchFactory(filterUsers, users).createSearch();
+        }
+
+        Filter filterPicture;
+        if (searchQuery.startsWith("#")) {
+            filterPicture = new SimiliarHashTags(searchQuery.substring(1));
+        } else {
+            filterPicture = new SimilarCaption(searchQuery);
+        }
+
+        List<ISearchable> pictures = (List<ISearchable>) (List<?>) pictureService.getAllPictures();
+        return new PictureSearchFactory(filterPicture, pictures).createSearch();
+
+    }
+    
+}
