9abb60f8df57309d192c252b57d11207973e492b
codetoby <tobymelcherds@gmail.com>
2024-03-26 00:44:50 +0100

    Add hash tag functionality to image upload

--- src/presentation/controllers/ImageUploadController.java
+++ src/presentation/controllers/ImageUploadController.java
@@ -1,17 +1,36 @@
 package src.presentation.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import src.application.services.HashTagService;
 import src.application.services.PictureService;
+import src.domain.entities.HashTag;
 import src.domain.entities.Picture;
 import src.domain.entities.User;
 
 public class ImageUploadController extends BaseController{
 
     private final PictureService pictureService;
+    private final HashTagService hashTagService;
 
     public ImageUploadController() {
         this.pictureService = new PictureService();
+        this.hashTagService = new HashTagService();
     }
-    public void uploadImage(User user, String imagePath, String caption) {
+
+    public void uploadImage(User user, String imagePath, String caption, String hashTags) {
+        Picture picture = new Picture(user, imagePath, caption);
+        String[] hashTagArray = hashTags.split(" ");
+        List<HashTag> hashTagList = new ArrayList<>();
+        for (String hashTag : hashTagArray) {
+            if (hashTag.startsWith("#")) {
+                hashTag = hashTag.substring(1);
+                hashTagList.add(new HashTag(picture, hashTag));
+            }
+        }
+        
+        hashTagService.saveHashTags(hashTagList);
         pictureService.savePicture(new Picture(user, imagePath, caption));
     }
 
